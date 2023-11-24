3fc5d3a31bfb40eab8b838403b310ff7eb3df823
GiuNogueiraSa <142549717+GiuNogueiraSa@users.noreply.github.com>
2023-11-24 10:50:02 -0300

    Update index.js

--- src/pages/PageModa/index.js
+++ src/pages/PageModa/index.js
@@ -3,7 +3,7 @@ import TextoModa from "../../componentes/TextoModa"
 import Header from '../../componentes/Header'
 import Footer from '../../componentes/Footer'
 import TextoCadastro from "../../componentes/TextoCadastro"
-import NewsletterForm from '../../componentes/email'
+
 const ModaContainer = styled.div`
 justify-content: center;
 background-color: #EFF2DD;
@@ -15,7 +15,7 @@ function PageModa() {
             <Header />
             <TextoModa/>
             <TextoCadastro/>
-            <NewsletterForm/>
+          
             <Footer/>
         </ModaContainer>
     )
